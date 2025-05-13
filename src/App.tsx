15475d8a73468accd19e46c63074a01c5d6df1ab
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-05-12 22:06:08 -0300

    implementado styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Olá mundo</h1>
-    </>
+    </ThemeProvider>
   );
 }
