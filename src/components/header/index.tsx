929d8222c40ebc6fe65c7022b68ed7bce4fc6b00
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-05-14 21:35:21 -0300

    Criado modal de nova transação

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../newtransactionmodal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import * as Dialog from '@radix-ui/react-dialog';
 
@@ -13,15 +14,7 @@ export function Header() {
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nota transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
